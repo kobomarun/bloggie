95b1ae92aa3bb64da7a950fa9e262aba2c2c113e
Olufemi Oluoje <lufemi.oluoje@54gene.com>
2023-09-12 06:50:46 +0100

    fix 404 error

--- app/Http/Controllers/Website/BlogController.php
+++ app/Http/Controllers/Website/BlogController.php
@@ -19,8 +19,9 @@ class BlogController extends Controller
         ]);
     }
 
-    public function show(Blog $blog)
+    public function show($id)
     {
+        $blog = Blog::find($id);
         return view('website.blog.show')->with([
             'blog' => $blog
         ]);
